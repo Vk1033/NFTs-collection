29c877496f01fdec14dec0fc72098a1831c4790f
Vk <kinkarvansh@gmail.com>
2025-07-29 14:47:21 +0000

    Add Interactions script and BasicNft test with minting functionality

---
+++ script/Interactions.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract Interactions is Script {
+    string public constant tokenUri =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function run() external {
+        address mostRecentlyDeployedBasicNft = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedBasicNft);
+    }
+
+    function mintNftOnContract(address basicNftAddress) public {
+        vm.startBroadcast();
+        BasicNft(basicNftAddress).mintNft(tokenUri);
+        vm.stopBroadcast();
+    }
+}
---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {
+    BasicNft private basicNft;
+    DeployBasicNft private deployer;
+
+    address private USER = makeAddr("user");
+    string public constant tokenUri =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() external {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameISCorrect() external view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+        assert(keccak256(abi.encodePacked(actualName)) == keccak256(abi.encodePacked(expectedName)));
+        assertEq(actualName, expectedName, "NFT name should be Dogie");
+    }
+
+    function testCanMintandHaveBalance() external {
+        vm.startPrank(USER);
+        basicNft.mintNft(tokenUri);
+        vm.stopPrank();
+
+        assertEq(basicNft.balanceOf(USER), 1, "User should have 1 NFT");
+        assertEq(basicNft.tokenURI(0), tokenUri, "Token URI should match the minted URI");
+    }
+}
